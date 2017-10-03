265c16272ee84c04b8de02496a7020e5f4756317
Chris Reid <cr01927@georgiasouthern.edu>
2017-10-03 14:15:39 -0400

    Uses past data instead of future
    Stores and publishes last received data, instead of waiting for next data to be received

--- src/pos_estimator/odom_acquisition_node.py
+++ src/pos_estimator/odom_acquisition_node.py
@@ -17,93 +17,46 @@ import tf
 from pos_estimator_msgs.msg import AcquireTrainData, AcquireEvalData, EstimatedPose, OdomEvalData, OdomTrainData
 from nav_msgs import Odometry
 
-
-# Variables to indicate that we need to store incoming estimated_pose and odometry data
-need_current_location = False
-need_odom_data = False
-
-# Variable to indicate whether we're acquiring training data or evaluation data
-# True - acquiring eval data; False - acquiring train data; None - not acquiring data
-need_evaluation_data = None
-
 # Variables to store incoming estimated_pose and odometry data
 current_location = EstimatedPose()
 odom_location = OdomEvalData()
 
-
-def publish_train_data(odom_data, cur_loc):
-    global need_evaluation_data
-    odom_train_data = OdomTrainData()
-    odom_train_data.odom_pose = odom_data.odom_pose
-    odom_train_data.estimated_pose = cur_loc.estimated_pose
-    odom_train_data_publisher.publish(odom_train_data)
-    need_evaluation_data = None
-
-
-def publish_eval_data(odom_data):
-    global need_evaluation_data
-    odom_eval_data_publisher.publish(odom_data)
-    need_evaluation_data = None
-
+# I think these need to be in the global namespace
+odom_eval_data_publisher = rospy.Publisher('odom_eval_data', OdomEvalData, queue_size=10)
+odom_train_data_publisher = rospy.Publisher('odom_train_data', OdomTrainData, queue_size=10)
 
 
 def callback_acquire_train_data(msg):
     # Do stuff when told to acquire training data
-    global need_evaluation_data = False
+	odom_train_data = OdomTrainData()
+	odom_train_data.odom_pose = odom_location.odom_pose
+	odom_train_data.estimated_pose = current_location.pose
+    global odom_train_data_publisher
+	odom_train_data_publisher.publish(odom_train_data)
 
 
 def callback_acquire_eval_data(msg):
     # Do stuff when told to acquire evaluation data
-    global need_evaluation_data = True
+    global odom_eval_data_publisher
+	odom_eval_data_publisher.publish(odom_location)
 
 
 def callback_current_location(msg):
-    # Do stuff when informed of a new current location
-    global current_location    
-
-    if (need_current_location == True):
-        # current_location is of type EstimatedPose
-        current_location = msg
-
-        need_current_location = False;
-        
-        # If we need training data (== False) and we've already gotten odom data
-        #  then we can publish
-        if (need_evaluation_data == False and need_odom_data == False):
-            
-            odom_train_data_publisher.publish(odom_location, current_location)
-            need_evaluation_data = None
-
+    global current_location = msg
 
 
 def callback_odom(msg):
     # Do stuff when new odometry data
-    global need_odom_data
-    global need_current_location
     global odom_location
-    global need_evaluation_data
 
-    if (need_odom_data == True):
-        # Calculate theta from the odometry quaternion
-        euler = tf.transformations.euler_from_quaternion(msg.pose.quaternion)
-        odom_location.odom_pose.theta = euler[2]
+	# Calculate theta from the odometry quaternion
+	euler = tf.transformations.euler_from_quaternion(msg.pose.quaternion)
+	odom_location.odom_pose.theta = euler[2]
 
-        # Copy x and y from the odometry pose into our EstimatedPose
-        odom_location.odom_pose.x = msg.pose.pose.position.x
-        odom_location.odom_pose.y = msg.pose.pose.position.y
+	# Copy x and y from the odometry pose into our EstimatedPose
+	odom_location.odom_pose.x = msg.pose.pose.position.x
+	odom_location.odom_pose.y = msg.pose.pose.position.y
 
-        # Indicate we've obtained the odom_data as requested
-        need_odom_data = False
-
-        # If we only needed evaluation data, publish OdomEvalData
-        if (need_evaluation_data == True):
-            publish_eval_data(odom_location)
-            return
-        # If we need training data and we have current_location,
-        #   publish OdomTrainData
-        elif (need_evaluation_data == False and need_current_location == False):
-            publish_train_data(odom_location, current_location)
-            return
             
 
 def odom_acquisition_node():
@@ -116,8 +69,7 @@ def odom_acquisition_node():
     rospy.Subscriber('current_location', EstimatedPose, callback_current_location)
     rospy.Subscriber('odom', Odometry, callback_odom)
 
-    odom_eval_data_publisher = rospy.Publisher('odom_eval_data', OdomEvalData, queue_size=10)
-    odom_train_data_publisher = rospy.Publisher('odom_train_data', OdomTrainData, queue_size=10)
+    
 
 
     while not rospy.is_shutdown():
